e5b32001f62d95b09e7ed735727b1a5bb61ccf92
Sabino-03 <elio.sab2003@gmail.com>
2026-06-17 12:49:00 +0200

    fix button component as reusable

--- src/app/components/button-component/button.component.ts
+++ src/app/components/button-component/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, input, InputSignal, OnInit, Output } from "@angular/core";
 
 @Component({
     selector: 'button-component',
@@ -6,8 +6,26 @@ import { Component } from "@angular/core";
     styleUrl: './button.css'
 })
 
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
 
-    constructor() {}
+  @Input() isActivated_btn : boolean = true; //ABILITA/DISABILITA IL BUTTON
+  label_btn : InputSignal<string> = input.required(); //TESTO DEL BUTTON
+  size_btn : InputSignal<'small' | 'medium' | 'large'> = input.required(); //DIMENSIONE DEL BUTTON
+  type_btn : InputSignal<'button' | 'reset'> = input.required(); //TIPO HTML
+  variant_btn : InputSignal<'log' | 'searchTerm' | 'operations' | 'deleteTerm'> = input.required(); //STILE CSS
+  @Output() onClick : EventEmitter<void> = new EventEmitter<void>();
+
+  //constructor() {}
+
+  ngOnInit() : void { console.log(`Button Initialized : Label:${this.label_btn}, Style:${this.size_btn}, ${this.variant_btn}`); }
+
+  enableButton() : void { this.isActivated_btn = true; } //ABILITA
+
+  disableButton() : void { this.isActivated_btn = false; } //DISABILITA
+
+  handleClick() : void { //GESTIONE
+    if(this.isActivated_btn === true)
+      this.onClick.emit();
+  }
 
 }
