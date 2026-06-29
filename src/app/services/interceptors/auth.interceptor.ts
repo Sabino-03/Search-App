799583714c05afd0945f75ae1d5f66a8b11a6059
Sabino-03 <elio.sab2003@gmail.com>
2026-06-29 22:10:56 +0200

    redefine authInterceptor according to latest version of angular

--- src/app/services/interceptors/auth.interceptor.ts
+++ src/app/services/interceptors/auth.interceptor.ts
@@ -1,61 +1,17 @@
-/*
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { catchError, first, mergeMap, Observable, of } from "rxjs";
+import { HttpHandlerFn, HttpInterceptorFn, HttpRequest } from "@angular/common/http";
+import { inject } from "@angular/core";
 import { AuthService } from "../auth.service";
 
-export class AuthInterceptor implements HttpInterceptor {
+export const AuthInterceptor : HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
 
-    public cloneReq : Observable<HttpEvent<any>> = new Observable();
+    const authService = inject(AuthService);
+    const token = authService.getToken();
 
-    constructor( private authService : AuthService ) {}
-    
-    //SI IMPLEMENTA METODO intercept -> ESSO RESTITUISCE UN OBSERVABLE
-    intercept(req: HttpRequest<any>, next: HttpHandler) : Observable<HttpEvent<any>> {
-        //req -> RICHIESTA ATTUALE CHE VIENE ESEGUITA
-        //next -> UTILITY PER PASSARE AL SUCCESSIVO interceptor
+    if(!token)
+        return next(req);
 
-        return this.authService.isLoggedIn$
-        .pipe(
-            first(),
-            mergeMap((token : boolean) => {
-                let cloneReq = req;
+    const cloneReq = req.clone({setHeaders : {Authorization : `Bearer ${token}`}});
 
-                if(token)
-                    cloneReq = req.clone( {setHeaders : {Authorization : `Bearer ${token}`}} );
-
-                return next.handle(cloneReq)
-                .pipe(
-                    catchError((err) => {
-
-                        console.log(err); //OUT : HTTPErrorResponse
-
-                        if(err instanceof HttpErrorResponse) {
-                            switch (err.status) {
-                                case 401 : {
-                                    //TOKEN SCADUTO
-                                }
-                                    break;
-
-                                case 404 : {
-                                    //SERVER DOWN
-
-                                    this.authService.removeToken();
-                                }
-                                    break;
-
-                                default : {}
-                            }
-
-                            return of(err);
-
-                        }
-
-                    })
-                )
-            })
-        )
-
-    }
+    return next(cloneReq);
 
 }
-*/
