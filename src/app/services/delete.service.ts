11bfe576f997e1596148709ebd57a968665df31f
Sabino-03 <elio.sab2003@gmail.com>
2026-06-29 13:24:31 +0200

    service deprecated

--- src/app/services/delete.service.ts
+++ src/app/services/delete.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from "@angular/core";
+import { inject, Injectable } from "@angular/core";
 import { NumberOfService } from "./numberOf.service";
 
+//SERVICE IL QUALE È UN INTERMEDIARIO (PROXY) INUTILE . [INCORPORA METODI CHE IMPLEMENTANO OPERAZIONI -> INUTILI]
+/*
 @Injectable({ providedIn: 'root' })
 export class DeleteService {
 
-  constructor( private numberOfService : NumberOfService ) {}
+  private numberOfService = inject(NumberOfService);
 
   resetCounters() {
     this.numberOfService.numOfUsers = 0 ;
@@ -12,3 +14,4 @@ export class DeleteService {
   }
 
 }
+*/
